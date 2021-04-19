9f5c5aae20d8ff4b6fade18cde84d4dde9164342
YJChan <yjtoro@hotmail.com>
2021-04-19 14:34:55 +0800

    update travis deploy pipeline

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -2,6 +2,7 @@
 openssl aes-256-cbc -K $encrypted_9f3b5599b056_key -iv $encrypted_9f3b5599b056_iv -in service-account.json.enc -out service-account.json -d
 
 curl https://sdk.cloud.google.com | bash > /dev/null;
+
 source $HOME/google-cloud-sdk/path.bash.inc
 
 # push docker image to registry
@@ -9,7 +10,12 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push $DOCKER_REPO/api-gateway:$GIT_SHA
 
 #gcloud deploy cloud run
-gcloud auth configure-docker
+gcloud auth activate-service-account --key-file service-account.json
+
+gcloud config set project $GCLOUD_PROJECT_ID
+
+gcloud config set compute/zone asia-southeast1-a
+# gcloud auth configure-docker
 
 docker pull $DOCKER_REPO/api-gateway:$GIT_SHA
 
