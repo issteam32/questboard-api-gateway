0cb26c0bde36ec34703e50baf9ccdcb70f8f4392
YJChan <yjtoro@hotmail.com>
2021-05-09 18:47:58 +0800

    local development continue

--- gateway/config.js
+++ gateway/config.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 
 module.exports = {
     apiServerUrl: process.env.API_SERVER,
-    userServiceUrl: `${process.env.API_SERVER}/user-service`,
-    questServiceUrl: `${process.env.API_SERVER}/quest-service`,
-    reviewServiceUrl: `${process.env.API_SERVER}/review-service`,
-    chatServiceUrl: `${process.env.API_SERVER}/chat`,
-    rewardServiceUrl: `${process.env.API_SERVER}/reward-service`,
+    userServiceUrl: `${process.env.API_SERVER}:30002`,
+    questServiceUrl: `${process.env.API_SERVER}:30005`,
+    reviewServiceUrl: `${process.env.API_SERVER}:30004`,
+    chatServiceUrl: `${process.env.API_SERVER}:30003`,
+    rewardServiceUrl: `${process.env.API_SERVER}:30007`,
     questServiceUrlLocal: 'http://192.168.49.2:30005'
 }
--- travis-deploy.sh
+++ travis-deploy.sh
@@ -10,20 +10,21 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push $DOCKER_REPO/api-gateway:$GIT_SHA
 
 #gcloud deploy cloud run
-gcloud auth activate-service-account --key-file service-account.json
+#disable CICD to production
+# gcloud auth activate-service-account --key-file service-account.json
 
-gcloud config set project $GCLOUD_PROJECT_ID
+# gcloud config set project $GCLOUD_PROJECT_ID
 
-gcloud config set compute/zone asia-southeast1-a
+# gcloud config set compute/zone asia-southeast1-a
 
-gcloud auth configure-docker
+# gcloud auth configure-docker
 
-docker pull $DOCKER_REPO/api-gateway:$GIT_SHA
+# docker pull $DOCKER_REPO/api-gateway:$GIT_SHA
 
-docker tag $DOCKER_REPO/api-gateway:$GIT_SHA gcr.io/$GCLOUD_PROJECT_ID/api-gateway:$GIT_SHA
+# docker tag $DOCKER_REPO/api-gateway:$GIT_SHA gcr.io/$GCLOUD_PROJECT_ID/api-gateway:$GIT_SHA
 
-docker push gcr.io/$GCLOUD_PROJECT_ID/api-gateway:$GIT_SHA
+# docker push gcr.io/$GCLOUD_PROJECT_ID/api-gateway:$GIT_SHA
 
-gcloud container images list-tags gcr.io/$GCLOUD_PROJECT_ID/api-gateway
+# gcloud container images list-tags gcr.io/$GCLOUD_PROJECT_ID/api-gateway
 
-gcloud run deploy $SERVICE_NAME --image gcr.io/$GCLOUD_PROJECT_ID/api-gateway:$GIT_SHA --platform managed --region asia-southeast1 --allow-unauthenticated
+# gcloud run deploy $SERVICE_NAME --image gcr.io/$GCLOUD_PROJECT_ID/api-gateway:$GIT_SHA --platform managed --region asia-southeast1 --allow-unauthenticated
